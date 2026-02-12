bdd217eaafa254e3bfb074be73039157000a5f68
qwjyh <urataw421@gmail.com>
2026-02-12 18:42:24 +0900

    chore: fix hidden lifetime clippy lint

--- src/main.rs
+++ src/main.rs
@@ -142,7 +142,7 @@ fn default_config_dir() -> Result<PathBuf> {
     Ok(config_dir)
 }
 
-fn find_last_commit(repo: &Repository) -> Result<Option<Commit>, git2::Error> {
+fn find_last_commit(repo: &'_ Repository) -> Result<Option<Commit<'_>>, git2::Error> {
     if repo.is_empty()? {
         Ok(None)
     } else {
